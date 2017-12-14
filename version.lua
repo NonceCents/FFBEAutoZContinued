b44931979ead1db36baf38e421ed48e39ef6f876
NonceCents <31529666+NonceCents@users.noreply.github.com>
2017-12-14 00:44:45 -0500

    Update to contain patch notes

--- version.lua
+++ version.lua
@@ -1,2 +1,5 @@
+patchNotes = "Features:\nNo new features this patch! Will likely be adding Lasswell eventually for explorations if there is demand.\n\nBug Fixes: \nRepeat mode should now work more reliably.\n\n"
+patchNotes = patchNotes.."\nI am aware of issues with certain MPs not reading properly; no solutions have presented themselves.\n\nI am requesting image transparency support from the AnkuLua author to resolve the issue.\n\n"
+
 --Returns the current version of FFBEAutoZ as first value, and the version of the image database as the second.
-return "20.0.5", "2"
+return "20.0.5", "2", patchNotes
