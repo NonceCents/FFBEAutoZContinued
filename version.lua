44579aeede49cad58319499f2d521e4d9d23d48a
NonceCents <31529666+NonceCents@users.noreply.github.com>
2017-12-14 00:29:13 -0500

    20.0.5

--- version.lua
+++ version.lua
@@ -1,2 +1,2 @@
---Returns the current version of FFBEAutoZ as first value, whether or not the image directory needs to be updated as the second.
-return "20.0.4", "2"
+--Returns the current version of FFBEAutoZ as first value, and the version of the image database as the second.
+return "20.0.5", "2"
