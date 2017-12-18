f3f19154557b643b65d62a6bcfca7a1725ee86b9
NonceCents <31529666+NonceCents@users.noreply.github.com>
2017-12-18 02:49:26 -0500

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,5 +1,5 @@
-patchNotes = "Features:\nNo new features this patch! Will likely be adding Lasswell eventually for explorations if there is demand.\n\nBug Fixes: \nRepeat mode should now work more reliably.\n\n"
+patchNotes = "Features:\nAdded Repair option.\n\nBug Fixes: \nRepeat mode fixes again... sorry I keep screwing this up. Arena Repeat mode should work now too, please report any problems.\n\n"
 patchNotes = patchNotes.."\nI am aware of issues with certain MPs not reading properly; no solutions have presented themselves.\n\nI am requesting image transparency support from the AnkuLua author to resolve the issue.\n\n"
 
 --Returns the current version of FFBEAutoZ as first value, and the version of the image database as the second.
-return "20.0.5", "2", patchNotes
+return "20.0.6", "2", patchNotes
